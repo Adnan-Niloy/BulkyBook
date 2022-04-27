3198d51b651d9258e88f67205fbe15c0b3ae0ac8
Adnan-Niloy <adnanniloy1313@gmail.com>
2022-04-27 13:30:44 +0600

    Generic Repository Implementation

---
+++ BulkyBook.DataAccess/Repository/Implementation/Repository.cs
@@ -0,0 +1,47 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BulkyBook.DataAccess.Repository.Implementation
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            _dbSet = _db.Set<T>();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = _dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = _dbSet;
+            return query.ToList();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+    }
+}
