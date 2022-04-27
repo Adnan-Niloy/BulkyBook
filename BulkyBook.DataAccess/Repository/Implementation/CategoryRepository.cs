87701c376cff325cca833c5aefc7d2e67d94022b
Adnan-Niloy <adnanniloy1313@gmail.com>
2022-04-27 13:37:07 +0600

    Category Repo

---
+++ BulkyBook.DataAccess/Repository/Implementation/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.Interface;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository.Implementation
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            _db.Update(category);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ BulkyBook.DataAccess/Repository/Interface/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository.Interface
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+
+        void Save();
+    }
+}
