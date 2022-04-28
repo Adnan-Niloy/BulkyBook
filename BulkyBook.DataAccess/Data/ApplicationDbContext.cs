051a782f6c9c422f053ed83ae01bdc6636e9c07e
Adnan-Niloy <adnanniloy1313@gmail.com>
2022-04-28 12:24:00 +0600

    Extending Identity User

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -16,5 +16,7 @@ namespace BulkyBook.DataAccess.Data
         public DbSet<CoverType> CoverTypes { get; set; }
 
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
